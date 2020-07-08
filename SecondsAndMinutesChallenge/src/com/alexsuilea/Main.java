dc3b5dfabd71b97bec2e4b48866e891fdc21528a
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-08 20:57:12 +0300

    Add files via upload

---
+++ SecondsAndMinutesChallenge/src/com/alexsuilea/Main.java
@@ -0,0 +1,45 @@
+package com.alexsuilea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getDurationString(65L,9L));
+        System.out.println(getDurationString(3909L));
+    }
+
+    public static String getDurationString(long minutes, long seconds){
+        if(minutes < 0 || seconds < 0 || seconds > 59){
+            return "Invalid Value";
+        } else {
+            long hours = minutes/60;
+            long remainingMinutes = minutes%60;
+
+            String hoursString = hours + "h ";
+            if(hours < 10){
+                hoursString = "0" + hoursString;
+            }
+
+            String minutesString = remainingMinutes + "m ";
+            if(remainingMinutes < 10){
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if(seconds < 10){
+                secondsString = "0" + secondsString;
+            }
+
+            return hoursString + minutesString + secondsString;
+        }
+    }
+
+    public static String getDurationString(long seconds){
+        if(seconds < 0){
+            return "Invalid Value";
+        } else {
+            long minutes = seconds/60;
+            long remainingSeconds = seconds%60;
+            return getDurationString(minutes, remainingSeconds);
+        }
+    }
+}
